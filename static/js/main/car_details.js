ad47d0da6c6a5446ce8670e6325b4ff15c25b21d
rizalfadlyash <rizalfalyash@gmail.com>
2024-06-04 01:04:16 +0700

    update sesi

--- static/js/main/car_details.js
+++ static/js/main/car_details.js
@@ -1,24 +1,27 @@
 $('#date').datepicker({
     orientation: "bottom",
-    autoclose:true,
-    startDate:'0d',
-    format:'yyyy-mm-dd',
-    todayHighlight:true
-})
+    autoclose: true,
+    startDate: '0d',
+    format: 'yyyy-mm-dd',
+    todayHighlight: true
+});
 
-function createTransaction(id_mobil){
-    var user = 'foo'
+function createTransaction(id_mobil) {
+    var token = sessionStorage.getItem('token'); 
     var hari = $('#hari').val();
     $.ajax({
-        url : '/api/create_transaction',
-        type : 'post',
-        data : {
-            user : user,
-            hari : hari,
-            id_mobil : id_mobil
+        url: '/api/create_transaction',
+        type: 'post',
+        data: {
+            token: token,
+            hari: hari,
+            id_mobil: id_mobil
         },
-        success : function (response){
-            window.location.replace(`/transaksi/${response.id}`)
+        success: function (response) {
+            window.location.replace(`/transaksi/${response.id}`);
+        },
+        error: function (xhr, status, error) {
+            console.error(xhr.responseText);
         }
-    })
+    });
 }

ec51236d7565d23f08e9301c70abd4e576b371d8
Moleen <maulana.syakhiya@gmail.com>
2024-06-05 19:06:02 +0700

    menambahkan fitur konversi currency

--- static/js/dashboard/tambahdata.js
+++ static/js/dashboard/tambahdata.js
@@ -1,4 +1,19 @@
 
+$(document).ready(function(){
+
+    $('#input-file').change(function (e) {
+        file = this.files[0];
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = function (event) {
+                $("#imgPreview").attr("src", event.target.result);
+                $("#imgPreview").removeAttr('hidden');
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+})
 
 function addData(){
 
--- static/js/main/main.js
+++ static/js/main/main.js
@@ -2,3 +2,13 @@ function logout(){
     window.location.replace('/')
     $.removeCookie('token');
 }
+
+$(document).ready(function(){
+    $('[data-target="currency"]').each(function(){
+        $(this).text(new Intl.NumberFormat("id",{
+            style:'currency',
+            currency:"IDR",
+            maximumFractionDigits:0
+        }).format(parseInt($(this).text())))
+    })
+})
